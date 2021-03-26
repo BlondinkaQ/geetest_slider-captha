4adc44791fd052490761f11d3533f195349c0d97
BlondinkaQ <artem2011as@ukr.net>
2021-03-26 23:05:06 +0200

    solve captha

---
+++ slider_solve.py
@@ -0,0 +1,97 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+import base64
+from solver import PuzleSolver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+driver = webdriver.Chrome('driver/chromedriver')
+
+def wait_for_selector(drivers, selector, by=By.XPATH, timeout=30, EC_condtion=None):
+    wait = WebDriverWait(drivers, timeout=timeout)
+    el = wait.until(lambda d: d.find_element(by, selector))
+    if EC_condtion:
+        el = wait.until(EC_condtion(by, selector))
+    return el
+
+
+def capcha(driver):
+    try:
+        iframe = driver.find_elements_by_tag_name('iframe')[0]
+        driver.switch_to.frame(iframe)
+
+        elem = wait_for_selector(driver, "//div[@class='geetest_btn']")
+        elem.click()
+
+        time.sleep(10)
+
+        canvas = driver.find_element_by_xpath("//canvas[2]")
+
+        canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
+
+        canvas_png = base64.b64decode(canvas_base64)
+
+        with open(r"imgcap/screenshot1.png", 'wb') as f:
+            f.write(canvas_png)
+
+        canvas1 = driver.find_element_by_xpath("//canvas[1]")
+
+        canvas_base641 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas1)
+
+        canvas_png1 = base64.b64decode(canvas_base641)
+
+        with open(r"imgcap/screenshot2.png", 'wb') as f:
+            f.write(canvas_png1)
+
+        solver = PuzleSolver("imgcap/screenshot1.png", "imgcap/screenshot2.png")
+        solution = solver.get_position()
+
+        button = driver.find_element_by_class_name('geetest_slider_button')
+        ActionChains(driver).click_and_hold(button).perform()
+        ActionChains(driver).move_by_offset(xoffset=solution, yoffset=0).perform()
+        time.sleep(1.41)
+        ActionChains(driver).release().perform()
+
+        try:
+            while True:
+                try:
+                    time.sleep(8)
+                    driver.execute_script("document.querySelector('.geetest_refresh_1').click()")
+                    time.sleep(8)
+
+                    canvas = driver.find_element_by_xpath("//canvas[2]")
+
+                    canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);",
+                                                          canvas)
+
+                    canvas_png = base64.b64decode(canvas_base64)
+
+                    with open(r"imgcap/screenshot1.png", 'wb') as f:
+                        f.write(canvas_png)
+
+                    canvas1 = driver.find_element_by_xpath("//canvas[1]")
+
+                    canvas_base641 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);",
+                                                           canvas1)
+
+                    canvas_png1 = base64.b64decode(canvas_base641)
+
+                    with open(r"imgcap/screenshot2.png", 'wb') as f:
+                        f.write(canvas_png1)
+
+                    solver = PuzleSolver("imgcap/screenshot1.png", "imgcap/screenshot2.png")
+                    solution = solver.get_position()
+
+                    button = driver.find_element_by_class_name('geetest_slider_button')
+                    ActionChains(driver).click_and_hold(button).perform()
+                    ActionChains(driver).move_by_offset(xoffset=solution, yoffset=0).perform()
+                    time.sleep(1.43)
+                    ActionChains(driver).release().perform()
+                except:
+                    break
+        except:
+            pass
+    except:
+        pass
